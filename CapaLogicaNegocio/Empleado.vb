e7efc0a0dca0c5d73d1b6b456754cbced5d45b2c
Erick Cruz <erickcroz2013@gmail.com>
2022-08-08 23:19:24 +0200

    agregando las funciones de ingresar actualizar y eliminar empleado

--- CapaLogicaNegocio/Empleado.vb
+++ CapaLogicaNegocio/Empleado.vb
@@ -4,10 +4,12 @@ Imports System.Data.SqlClient
 Public Class Empleado
     Private DB As New CapaDato.DB
 
-    Private comando As New SqlCommand()
+    Private comando As SqlCommand
     Private leer As SqlDataReader
-    Private tabla As New DataTable()
+    Private tabla As DataTable
     Public Function ListarEmpleadoId(Id As String) As DataTable
+        tabla = New DataTable()
+        comando = New SqlCommand()
         comando.Connection = DB.Abrir()
         comando.CommandType = CommandType.StoredProcedure
         comando.CommandText = "sp_listar_empleado_id"
@@ -26,6 +28,8 @@ Public Class Empleado
         Return tabla
     End Function
     Public Function ListarEmpleadoDocumento(Documento As String) As DataTable
+        tabla = New DataTable()
+        comando = New SqlCommand()
         comando.Connection = DB.Abrir()
         comando.CommandType = CommandType.StoredProcedure
         comando.CommandText = "sp_listar_empleado_documento"
@@ -43,6 +47,8 @@ Public Class Empleado
         Return tabla
     End Function
     Public Function ListarEmpleado() As DataTable
+        tabla = New DataTable()
+        comando = New SqlCommand()
         comando.Connection = DB.Abrir()
         comando.CommandType = CommandType.StoredProcedure
         comando.CommandText = "sp_listar_empleado"
@@ -56,5 +62,92 @@ Public Class Empleado
             MsgBox(ex.Message)
         End Try
         Return tabla
+    End Function
+    Public Function LlenarCMB() As DataTable
+        tabla = New DataTable()
+        comando = New SqlCommand()
+        comando.Connection = DB.Abrir()
+        comando.CommandType = CommandType.StoredProcedure
+        comando.CommandText = "sp_llenar_cmb_documento_id"
+
+        Try
+            leer = comando.ExecuteReader()
+            tabla.Load(leer)
+            leer.Close()
+            comando.Connection = DB.Cerrar()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return tabla
+    End Function
+    Public Function InsertarEmpleado(Nombre As String, Apellido As String, Tipo As String, Documento As String, SalarioBase As String, Ingreso As Date, Direccion As String) As Integer
+        Dim res As Integer
+        comando = New SqlCommand()
+        comando.Connection = DB.Abrir()
+        comando.CommandType = CommandType.StoredProcedure
+        comando.CommandText = "sp_insertar_empleado_documento"
+        comando.Parameters.AddWithValue("@Estado", 1)
+        comando.Parameters.AddWithValue("@Nombres", Nombre)
+        comando.Parameters.AddWithValue("@Apellidos", Apellido)
+        comando.Parameters.AddWithValue("@Tipo", Tipo)
+        comando.Parameters.AddWithValue("@documento", Documento)
+        comando.Parameters.AddWithValue("@SalarioBase", Convert.ToDouble(SalarioBase))
+        comando.Parameters.AddWithValue("@Ingreso", Ingreso)
+        comando.Parameters.AddWithValue("@Direccion", Direccion)
+        Try
+            res = comando.ExecuteNonQuery()
+            comando.Parameters.Clear()
+            comando.Connection = DB.Cerrar()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return res
+    End Function
+    Public Function ActualizarEmpleado(Id As String, Nombre As String, Apellido As String, Tipo As String, Documento As String, SalarioBase As String, Ingreso As Date, Direccion As String) As Integer
+        Dim res As Integer
+        comando = New SqlCommand()
+        comando.Connection = DB.Abrir()
+        comando.CommandType = CommandType.StoredProcedure
+        comando.CommandText = "sp_actualizar_empleado"
+        comando.Parameters.AddWithValue("@Id", Id)
+        comando.Parameters.AddWithValue("@Estado", 1)
+        comando.Parameters.AddWithValue("@Nombres", Nombre)
+        comando.Parameters.AddWithValue("@Apellidos", Apellido)
+        comando.Parameters.AddWithValue("@Tipo", Tipo)
+        comando.Parameters.AddWithValue("@documento", Documento)
+        comando.Parameters.AddWithValue("@SalarioBase", Convert.ToDouble(SalarioBase))
+        comando.Parameters.AddWithValue("@Ingreso", Ingreso)
+        comando.Parameters.AddWithValue("@Direccion", Direccion)
+        Try
+            res = comando.ExecuteNonQuery()
+            comando.Parameters.Clear()
+            comando.Connection = DB.Cerrar()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return res
+    End Function
+
+    Public Function ElimiarEmpleado(Id As String) As Integer
+        Dim res As Integer
+        comando = New SqlCommand()
+        comando.Connection = DB.Abrir()
+        comando.CommandType = CommandType.StoredProcedure
+        comando.CommandText = "sp_Eliminar_empleado_id"
+        comando.Parameters.AddWithValue("@Id", Id)
+        Try
+            res = comando.ExecuteNonQuery()
+            comando.Parameters.Clear()
+            comando.Connection = DB.Cerrar()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        Return res
     End Function
 End Class
