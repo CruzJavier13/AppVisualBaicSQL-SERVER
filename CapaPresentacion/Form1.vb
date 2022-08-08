64643a96d68443773439ff6e06416f92771d9256
Erick Cruz <erickcroz2013@gmail.com>
2022-08-08 19:58:28 +0200

    agregando funcionalidades a los botones y al los textbox

--- CapaPresentacion/Form1.vb
+++ CapaPresentacion/Form1.vb
@@ -26,4 +26,78 @@ Public Class frmEmpleado
         End Try
         Return dgvEmpleado
     End Function
+
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        btnNuevo.Enabled = False
+        btnCancelar.Enabled = True
+        btnGuardar.Enabled = True
+
+
+        HabulitarTextBox()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+
+
+
+        btnNuevo.Enabled = True
+        btnGuardar.Enabled = False
+        btnEliminar.Enabled = False
+        btnCancelar.Enabled = False
+        LimpiarTextBox()
+        DesabilitarTextBox()
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+
+
+        btnNuevo.Enabled = True
+        btnGuardar.Enabled = False
+        btnEliminar.Enabled = False
+        btnCancelar.Enabled = False
+
+        LimpiarTextBox()
+        DesabilitarTextBox()
+    End Sub
+
+    Public Sub LimpiarTextBox()
+        txtId.Text = ""
+        txtEstado.Text = ""
+        txtNombres.Text = ""
+        txtApellidos.Text = ""
+        txtDocumento.Text = ""
+        txtSalarioBase.Text = ""
+        txtDireccion.Text = ""
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+
+        btnEliminar.Enabled = False
+        btnNuevo.Enabled = True
+        btnCancelar.Enabled = False
+        btnGuardar.Enabled = False
+
+        LimpiarTextBox()
+        DesabilitarTextBox()
+    End Sub
+
+    Public Sub HabulitarTextBox()
+        txtNombres.Enabled = True
+        txtApellidos.Enabled = True
+        cmbTipo.Enabled = True
+        txtDocumento.Enabled = True
+        txtSalarioBase.Enabled = True
+        dtpIngreso.Enabled = True
+        txtDireccion.Enabled = True
+    End Sub
+    Public Sub DesabilitarTextBox()
+        txtNombres.Enabled = False
+        txtApellidos.Enabled = False
+        cmbTipo.Enabled = False
+        txtDocumento.Enabled = False
+        txtSalarioBase.Enabled = False
+        dtpIngreso.Enabled = False
+        txtDireccion.Enabled = False
+    End Sub
 End Class
