180d178d1c454114b5e4899e579a2d7a8c2ec37d
Erick Cruz <erickcroz2013@gmail.com>
2022-08-08 18:36:44 +0200

    agregando la clase que modificara la tabla empleado

---
+++ CapaLogicaNegocio/Empleado.vb
@@ -0,0 +1,27 @@
+﻿Imports CapaDato
+Imports System.Data
+Imports System.Data.SqlClient
+Public Class Empleado
+    Private DB As New CapaDato.DB
+
+    Private comando As New SqlCommand()
+    Private leer As SqlDataReader
+    Private tabla As New DataTable()
+    Public Function ListarEmpleado(Id As String) As DataTable
+        comando.Connection = DB.Abrir()
+        comando.CommandType = CommandType.StoredProcedure
+        comando.CommandText = "sp_listar_empleado_id"
+        comando.Parameters.AddWithValue("@Id", Id)
+
+        Try
+            leer = comando.ExecuteReader()
+            tabla.load(leer)
+            comando.Connection = DB.Cerrar()
+            comando.Parameters.Clear()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Return tabla
+    End Function
+
+End Class
