277b8aa11d3c68c3e5311b22b6e312c82258b9b3
Erick Cruz <erickcroz2013@gmail.com>
2022-08-08 18:18:41 +0200

    agregando la capa dato al proyecto, clase DB coneccion a la base de dato

---
+++ CapaDato/DB.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+Public Class DB
+    Private coneccion As String = "Data Source=.;Initial Catalog=Empleados;Integrated Security=True"
+    Private strinDB As New SqlConnection(coneccion)
+
+    Public Function Abrir() As SqlConnection
+        If strinDB.State = ConnectionState.Closed Then
+            strinDB.Open()
+        End If
+        Return strinDB
+    End Function
+
+    Public Function Cerrar() As SqlConnection
+        If strinDB.State = ConnectionState.Open Then
+            strinDB.Close()
+        End If
+        Return strinDB
+    End Function
+End Class
